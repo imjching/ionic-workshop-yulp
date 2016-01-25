4d58c43a797d2b0ad400f9dbeee32b3a70bc8efe
Jay <imjching@hotmail.com>
2016-01-25 18:50:29 +0800

    added google maps to the feed details view

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'yulpApp' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('yulpApp', ['ionic'])
+angular.module('yulpApp', ['ionic', 'uiGmapgoogle-maps'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -22,6 +22,21 @@
 
     YelpAPI.searchBusiness($stateParams.businessId, function(data) {
       vm.business = data;
+      vm.map = {
+        center: {
+          latitude: data.location.coordinate.latitude,
+          longitude: data.location.coordinate.longitude
+        },
+        zoom: 14,
+        options: { scrollwheel : false }
+      };
+      vm.marker = {
+        center: {
+          latitude: data.location.coordinate.latitude,
+          longitude: data.location.coordinate.longitude
+        },
+        options: { draggable : false }
+      };
     });
   }
 
