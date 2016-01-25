7fb69463e319844e251a7cf2f7ef7f66ad3a2813
Jay <imjching@hotmail.com>
2016-01-25 15:06:49 +0800

    feed details layout with card

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,8 @@
   'use strict';
 
   angular.module('yulpApp')
-    .controller('FeedCtrl', ['YelpAPI', FeedCtrl]);
+    .controller('FeedCtrl', ['YelpAPI', FeedCtrl])
+    .controller('FeedDetailsCtrl', ['$stateParams', 'YelpAPI', FeedDetailsCtrl]);
 
   function FeedCtrl(YelpAPI) {
     var vm = this; // view model (vm)
@@ -12,6 +13,20 @@
       vm.businesses = res.data.businesses;
 
       console.log(vm.businesses);
+    });
+  }
+
+  function FeedDetailsCtrl($stateParams, YelpAPI) {
+    var vm = this; // view model (vm)
+
+    vm.businessId = $stateParams.businessId;
+    console.log(vm.businessId);
+
+    // load the first data first, temporary
+    YelpAPI.getData().then(function(res) {
+      vm.businesses = res.data.businesses;
+
+      vm.business = vm.businesses[0]; // temporary
     });
   }
 })();
